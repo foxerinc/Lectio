0c01ef83364d85a2c89daf9373b2bae186ccd84b
foxerinc <78334731+foxerinc@users.noreply.github.com>
2025-04-14 20:21:34 +0700

    Delete review entity

--- app/src/main/java/com/booktracker/lectio/data/datasource/local/database/LectioDatabase.kt
+++ app/src/main/java/com/booktracker/lectio/data/datasource/local/database/LectioDatabase.kt
@@ -5,15 +5,13 @@ import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.booktracker.lectio.data.datasource.local.dao.BookDao
 import com.booktracker.lectio.data.datasource.local.dao.GenreDao
-import com.booktracker.lectio.data.datasource.local.dao.ReviewDao
 import com.booktracker.lectio.data.datasource.local.entity.BookEntity
 import com.booktracker.lectio.data.datasource.local.entity.BookGenreCrossRef
 import com.booktracker.lectio.data.datasource.local.entity.GenreEntity
-import com.booktracker.lectio.data.datasource.local.entity.ReviewEntity
 import com.booktracker.lectio.utilis.BookStatusTypeConverter
 
 @Database(
-    entities = [BookEntity::class, GenreEntity::class, BookGenreCrossRef::class, ReviewEntity::class],
+    entities = [BookEntity::class, GenreEntity::class, BookGenreCrossRef::class],
     version = 1,
     exportSchema = false
 )
@@ -21,6 +19,4 @@ import com.booktracker.lectio.utilis.BookStatusTypeConverter
 abstract class LectioDatabase: RoomDatabase() {
     abstract val bookDao: BookDao
     abstract val genreDao: GenreDao
-    abstract val reviewDao: ReviewDao
-
 }
