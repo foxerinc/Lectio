4d8bb0670e1e1f4dc79f8a73e2eafbc477b07d08
foxerinc <78334731+foxerinc@users.noreply.github.com>
2025-04-14 15:09:20 +0700

    Update GetCurrentlyReadingBooksUseCase.kt

--- app/src/main/java/com/booktracker/lectio/domain/usecase/GetCurrentlyReadingBooksUseCase.kt
+++ app/src/main/java/com/booktracker/lectio/domain/usecase/GetCurrentlyReadingBooksUseCase.kt
@@ -1,12 +1,13 @@
 package com.booktracker.lectio.domain.usecase
 
-import com.booktracker.lectio.data.datasource.local.entity.BookWithGenresEntity
+
+import com.booktracker.lectio.domain.model.BookWithGenres
 import com.booktracker.lectio.domain.repository.BookRepository
 import com.booktracker.lectio.utilis.BookStatusType
 import kotlinx.coroutines.flow.Flow
 
 class GetCurrentlyReadingBooksUseCase (private val repository: BookRepository) {
-    operator fun invoke(): Flow<List<BookWithGenresEntity>> {
+    operator fun invoke(): Flow<List<BookWithGenres>> {
         return repository.getBooksByStatus(BookStatusType.CURRENTLY_READING)
     }
 }
