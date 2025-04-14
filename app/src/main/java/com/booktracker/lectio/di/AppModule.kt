a8ecc1b6a2fc4d5597c066f0d49cf4635b657f59
foxerinc <78334731+foxerinc@users.noreply.github.com>
2025-04-14 20:24:32 +0700

    update repository binds and create data class for bookusecases

--- app/src/main/java/com/booktracker/lectio/di/AppModule.kt
+++ app/src/main/java/com/booktracker/lectio/di/AppModule.kt
@@ -3,6 +3,12 @@ package com.booktracker.lectio.di
 import android.content.Context
 import androidx.room.Room
 import com.booktracker.lectio.data.datasource.local.database.LectioDatabase
+import com.booktracker.lectio.data.repository.BookRepositoryImpl
+import com.booktracker.lectio.data.repository.GenreRepositoryImpl
+import com.booktracker.lectio.domain.repository.BookRepository
+import com.booktracker.lectio.domain.repository.GenreRepository
+import com.booktracker.lectio.domain.usecase.BookUseCases
+import com.booktracker.lectio.domain.usecase.GetCurrentlyReadingBooksUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -27,6 +33,26 @@ object AppModule {
             .build()
     }
 
+    @Provides
+    @Singleton
+    fun provideBookRepository(database: LectioDatabase): BookRepository {
+        return BookRepositoryImpl(database)
+    }
+
+    @Provides
+    @Singleton
+    fun provideGenreRepository(database: LectioDatabase): GenreRepository {
+        return GenreRepositoryImpl(database)
+    }
+
+    @Provides
+    @Singleton
+    fun provideBookUseCases(repository: BookRepository): BookUseCases {
+        return BookUseCases(
+            getCurrentlyReadingBooks = GetCurrentlyReadingBooksUseCase(repository),
+
+        )
+    }
 
 
 
---
+++ app/src/main/java/com/booktracker/lectio/domain/usecase/BookUseCases.kt
@@ -0,0 +1,6 @@
+package com.booktracker.lectio.domain.usecase
+
+data class BookUseCases(
+    val getCurrentlyReadingBooks: GetCurrentlyReadingBooksUseCase,
+)
+
