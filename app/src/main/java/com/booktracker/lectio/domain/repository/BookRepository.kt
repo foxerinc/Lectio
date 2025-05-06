d5f5b61e10030af0539f23169bc0046154b458d7
foxerinc <78334731+foxerinc@users.noreply.github.com>
2025-05-06 16:21:54 +0700

    adding new function to book and genre repository

--- app/src/main/java/com/booktracker/lectio/domain/repository/BookRepository.kt
+++ app/src/main/java/com/booktracker/lectio/domain/repository/BookRepository.kt
@@ -3,16 +3,18 @@ package com.booktracker.lectio.domain.repository
 
 import com.booktracker.lectio.domain.model.Book
 import com.booktracker.lectio.domain.model.BookWithGenres
-import com.booktracker.lectio.utilis.BookStatusType
+import com.booktracker.lectio.utils.BookStatusType
 import kotlinx.coroutines.flow.Flow
 
 interface BookRepository {
     fun getAllBooks(): Flow<List<BookWithGenres>>
     fun getBooksByStatus(status: BookStatusType?): Flow<List<BookWithGenres>>
-    suspend fun insertBook(book: Book)
+    fun getFavoriteBooks(): Flow<List<BookWithGenres>>
+    fun getBookWithGenresById(id: Int): Flow<BookWithGenres>
+    suspend fun insertBook(book: Book) : Long
     suspend fun updateBook(book: Book)
     suspend fun deleteBook(book: Book)
-    suspend fun getBookById(id: Int): Book?
+    suspend fun deleteAllBooks()
     suspend fun updateCurrentPage(bookId: Int, page: Int)
     suspend fun updateFavorite(bookId: Int, favorite: Boolean)
 }
--- app/src/main/java/com/booktracker/lectio/domain/repository/GenreRepository.kt
+++ app/src/main/java/com/booktracker/lectio/domain/repository/GenreRepository.kt
@@ -6,7 +6,9 @@ import kotlinx.coroutines.flow.Flow
 interface GenreRepository {
     fun getAllGenres(): Flow<List<Genre>>
     suspend fun insertGenre(genre: Genre): Long
+    suspend fun updateGenreCrossRef(bookId: Int,genreId: Int)
     suspend fun getGenreById(id: Int): Genre?
+    suspend fun getGenreByName(name: String): Genre?
     suspend fun assignGenreToBook(bookId: Int, genreId: Int)
     suspend fun clearGenresForBook(bookId: Int)
 }
