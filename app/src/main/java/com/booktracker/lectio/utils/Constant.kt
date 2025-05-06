d5f2dad8e6efa8994cf43fe5c2643e9c534bbbe7
foxerinc <78334731+foxerinc@users.noreply.github.com>
2025-05-06 16:25:31 +0700

    Create Constant.kt

---
+++ app/src/main/java/com/booktracker/lectio/utils/Constant.kt
@@ -0,0 +1,11 @@
+package com.booktracker.lectio.utils
+
+const val WANT_TO_READ_VALUE = "Want to Read"
+const val CURRENTLY_READING_VALUE = "Currently Reading"
+const val FINISHED_READING_VALUE = "Finished Reading"
+
+const val NOTIFICATION_CHANNEL_NAME = "Course Channel"
+const val NOTIFICATION_CHANNEL_ID = "notify-schedule"
+const val NOTIFICATION_ID = 32
+const val ID_REPEATING = 101
+const val BOOK_ID = "book_id"
