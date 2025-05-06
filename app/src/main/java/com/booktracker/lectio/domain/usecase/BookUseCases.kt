9c9759464f1e6209ab24cd5966268a4252bb98cd
foxerinc <78334731+foxerinc@users.noreply.github.com>
2025-05-06 16:22:04 +0700

    Update BookUseCases.kt

--- app/src/main/java/com/booktracker/lectio/domain/usecase/BookUseCases.kt
+++ app/src/main/java/com/booktracker/lectio/domain/usecase/BookUseCases.kt
@@ -2,5 +2,14 @@ package com.booktracker.lectio.domain.usecase
 
 data class BookUseCases(
     val getBooksByStatusUseCase: GetBooksByStatusUseCase,
+    val addBookWithGenreUseCase: AddBookWithGenreUseCase,
+    val getAllBookUseCase: GetAllBookUseCase,
+    val getFavoriteBookUseCase: GetFavoriteBookUseCase,
+    val getBookWithGenresByIdUseCase: GetBookWithGenresByIdUseCase,
+    val updateBookUseCase: UpdateBookWithGenreUseCase,
+    val deleteBookUseCase: DeleteBookUseCase,
+    val deleteAllBookUseCase: DeleteAllBookUseCase,
+    val updateBookFavoriteStatusUseCase: UpdateBookFavoriteStatusUseCase,
+
 )
 
